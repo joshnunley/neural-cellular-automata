3e08ae0421a231166d1d09b5d7d7243e52f47cff
Joshua Nunley <joshnunl@i9.red.uits.iu.edu>
2022-08-25 13:24:06 -0400

    image util functions file

---
+++ EmojiUtils.py
@@ -0,0 +1,77 @@
+# Packages
+import PIL.Image, PIL.ImageDraw
+import requests
+import numpy as np
+from tensorflow import clip_by_value
+
+# STDLib
+import io
+import base64
+
+# Local
+from Constants import TARGET_SIZE, TARGET_EMOJI
+
+
+def imwrite(f, a, fmt=None):
+    a = np.asarray(a)
+    if isinstance(f, str):
+        fmt = f.rsplit(".", 1)[-1].lower()
+        if fmt == "jpg":
+            fmt = "jpeg"
+        f = open(f, "wb")
+    np2pil(a).save(f, fmt, quality=95)
+
+
+def imencode(a, fmt="jpeg"):
+    a = np.asarray(a)
+    if len(a.shape) == 3 and a.shape[-1] == 4:
+        fmt = "png"
+    f = io.BytesIO()
+    imwrite(f, a, fmt)
+    return f.getvalue()
+
+
+def im2url(a, fmt="jpeg"):
+    encoded = imencode(a, fmt)
+    base64_byte_string = base64.b64encode(encoded).decode("ascii")
+    return "data:image/" + fmt.upper() + ";base64," + base64_byte_string
+
+
+def load_image(url, max_size=TARGET_SIZE):
+    r = requests.get(url)
+    img = PIL.Image.open(io.BytesIO(r.content))
+    img.thumbnail((max_size, max_size), PIL.Image.ANTIALIAS)
+    img = np.float32(img) / 255.0
+    # premultiply RGB by Alpha
+    img[..., :3] *= img[..., 3:]
+    return img
+
+
+def load_emoji(emoji):
+    code = hex(ord(emoji))[2:].lower()
+    url = (
+        "https://github.com/googlefonts/noto-emoji/blob/main/png/128/emoji_u%s.png?raw=true"
+        % code
+    )
+    return load_image(url)
+
+
+def to_rgba(x):
+    return x[..., :4]
+
+
+def to_alpha(x):
+    return tf.clip_by_value(x[..., 3:4], 0.0, 1.0)
+
+
+def to_rgb(x):
+    # assume rgb premultiplied by alpha
+    rgb, a = x[..., :3], to_alpha(x)
+    return 1.0 - a + rgb
+
+
+if __name__ == "__main__":
+    print("here")
+    
+    emoji_img = load_emoji(TARGET_EMOJI)
+    
